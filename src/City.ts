612d69c5bcb530d930a01db30e2c458a4c425133
Jake Snipes <jsnipes@seas.upenn.edu>
2019-04-02 22:07:32 -0400

    Implemented random point placement and selection

--- src/City.ts
+++ src/City.ts
@@ -3,11 +3,15 @@ import {vec2, vec3, mat4} from 'gl-matrix';
 export default class City {
     roadInfo : Road[];
     cellSize : number; // Smaller cell size => Higher resolution
-    invCellSize : number; // 1 / cellSize
+    invCellSize : number; // 1.0 / cellSize
     validityGrid : boolean[];
+    nValid : number;
+    nBuildings : number; // Number of buildings to randomly place in the scene
+    allRandomPoints : vec2[];
+    selectedRandomPoints : vec2[];
     roadTransfs : mat4[];
 
-    constructor(cellSize?: number) {
+    constructor(cellSize?: number, nBuildings?: number) {
         if (cellSize == undefined) {
             this.cellSize = 0.5;
             this.invCellSize = 2.0;
@@ -17,10 +21,27 @@ export default class City {
             this.invCellSize = 1.0 / cellSize.valueOf();
         }
 
+        if (nBuildings == undefined) {
+            this.nBuildings = 20;
+        }
+        else {
+            this.nBuildings = nBuildings;
+        }
+
+        this.nValid = 0;
+
         this.roadInfo = [];
         this.roadTransfs = this.drawRoadGrid();
+
         this.validityGrid = [];
         this.generateValidityGrid();
+
+        this.allRandomPoints = [];
+        this.generateRandomPoints();
+
+        this.selectedRandomPoints = [];
+        this.selectPoints();
+        console.log(this.selectedRandomPoints.length);
     }
 
     // Draw a lattice of roads using instanced rendering
@@ -37,8 +58,10 @@ export default class City {
 
             transfs.push(mat4.clone(m));
             
-            // TODO: Fix based on info
-            this.roadInfo.push(new Road(vec2.fromValues(0, 0), vec2.fromValues(1, 1)));
+            // "Start" and "end" here represent the thickness of the road
+            // It spans the entirety of the plane along the x direction
+            let p : number = i * 6.0 - 43.0;
+            this.roadInfo.push(new Road(vec2.fromValues(-50, p - 0.1), vec2.fromValues(50, p + 0.1)));
         }
 
         // Vertical
@@ -50,16 +73,18 @@ export default class City {
 
             transfs.push(mat4.clone(m));
 
-            // TODO: Fix
-            this.roadInfo.push(new Road(vec2.fromValues(0,0), vec2.fromValues(1,1)));
+            // Same as above but spans entirety of plane along z direction
+            let p : number = i * 6.0 - 45.0;
+            this.roadInfo.push(new Road(vec2.fromValues(p - 0.1, -50), vec2.fromValues(p + 0.1, 50)));
         }
 
         return transfs;
     }
 
     generateValidityGrid() {
-        for (let i = -50 * this.invCellSize; i <= 50 * this.invCellSize; i += this.cellSize) {
-            for (let j = -50 * this.invCellSize; j <= 50 * this.invCellSize; j += this.cellSize) {
+        // Grid size is (100 * invCellSize + 1) x (100 * invCellSize + 1)
+        for (let i = -50 * this.invCellSize; i <= 50 * this.invCellSize; ++i) {
+            for (let j = -50 * this.invCellSize; j <= 50 * this.invCellSize; ++j) {
                 let currPos : vec2 = vec2.fromValues(i * this.cellSize, j * this.cellSize);
                 let currValid : boolean = true; 
                 // For each road...               
@@ -69,26 +94,123 @@ export default class City {
                     let s : vec2 = Road.floor(r.start);
                     let e : vec2 = Road.floor(r.end);
                     // If our current cell is one of those, mark it false (invalid)...
-                    if (currPos >= s || currPos <= e) {
+                    if (currPos >= s || currPos <= e || this.isWater(currPos)) {
                         currValid = false;
-                        break;
+                        break; // Only breaks out of k loop
                     }
                 }
                 // ...else, mark it true (valid)
                 this.validityGrid.push(currValid.valueOf());
             }
         }
+        console.log(this.validityGrid.length);
     }
 
     // Flatten a 2D index to 1D to appropriately access the validity array
     getValidityAt(p: vec2) : boolean {
         let i : number = p[0] + (100 * this.invCellSize + 1) * p[1];
-        return this.validityGrid[i].valueOf();
+        return this.validityGrid[i];
     }
 
-    // TODO: Random point generation (don't do FPD unless you want to)
+    flatten(p: vec2) : number {
+        return p[0] + (100 * this.invCellSize + 1) * p[1];
+    }
+
+    // Random point generation within each valid cell
+    generateRandomPoints() {
+        for (let i = 0; i < this.validityGrid.length; ++i) {
+            if (this.validityGrid[i]) {
+                let x : number = Math.floor(i / (100 * this.invCellSize + 1));
+                let y : number = i % (100 * this.invCellSize + 1);
+                this.allRandomPoints.push(vec2.fromValues(x + this.cellSize * Math.random(), y + this.cellSize * Math.random()));
+            }
+        }
+    }
+
+    // Randomly select some of the points
+    selectPoints() {
+        for (let i = 0; i < this.nBuildings; ++i) {
+            let randIdx : number = Math.floor(this.allRandomPoints.length * Math.random());
+            this.selectedRandomPoints.push(this.allRandomPoints[randIdx]);
+        }
+    }
+
+/////////////// Access terrain info on CPU ///////////////
+    hash2D(x: vec2) : number {
+        let i : number = vec2.dot(x, vec2.fromValues(123.4031, 46.5244876));
+        let j : number = Math.sin(i * 7.13) * 268573.103291;
+        return j - Math.floor(j);
+    }
+
+    vecFract(a: vec2) : vec2 {
+        return vec2.fromValues(a[0] - Math.floor(a[0]), a[1] - Math.floor(a[1]));
+    }
+
+    mix(a: number, b: number, t: number) : number {
+        return a * (1.0 - t) + b * t;
+    }
+
+    // 2D noise
+    noise(p: vec2) : number {
+        let corner : vec2 = vec2.create();
+        vec2.floor(corner, p);
+        let inCell : vec2 = this.vecFract(p);
+
+        let brCorner : vec2 = vec2.create();
+        vec2.add(brCorner, vec2.clone(corner), vec2.fromValues(1.0, 0.0));
+
+        let bL : number = this.hash2D(corner);
+        let bR : number = this.hash2D(brCorner);
+        let bottom : number = this.mix(bL, bR, inCell[0]);
+
+        let tCorner : vec2 = vec2.create();
+        vec2.add(tCorner, vec2.clone(corner), vec2.fromValues(0.0, 1.0));
+        let trCorner : vec2 = vec2.create();
+        vec2.add(trCorner, tCorner, vec2.fromValues(1.0, 0.0));
+
+        let tL : number = this.hash2D(tCorner);
+        let tR : number = this.hash2D(trCorner);
+        let top : number = this.mix(tL, tR, inCell[0]);
+
+        return this.mix(bottom, top, inCell[0]);
+    }
+
+    fbm(q: vec2) : number {
+        let acc : number = 0.0;
+        let freqScale : number = 2.0;
+        let invScale : number = 1.0 / freqScale;
+        let freq : number = 1.0;
+        let amp : number = 1.0;
+
+        for (let i = 0; i < 3; ++i) {
+            freq *= freqScale;
+            amp *= invScale;
+            let qScale : vec2 = vec2.create();
+            vec2.scale(qScale, q, freq);
+            acc += this.noise(qScale) * amp;
+        }
+        return acc;
+    }
+
+    // Terrain height map computed using multi-octave 2D FBM
+    getTerrain(q: vec2) : number {
+        let fbm1 : vec2 = vec2.create();
+        vec2.subtract(fbm1, vec2.clone(q), vec2.fromValues(0.2, 0.2));
+
+        let fbm2 : vec2 = vec2.create();
+        vec2.add(fbm2, vec2.clone(q), vec2.fromValues(25.2, -22.8));
+
+        let p : vec2 = vec2.fromValues(this.fbm(fbm1), this.fbm(fbm2));
+        return Math.min(Math.max(2.0 * this.fbm(p) - 0.3, 0.0), 1.0);
+    }
+
+    // Check if terrain ahead is water or land
+    isWater(q: vec2) : boolean {
+        return (this.getTerrain(q) < 0.57);
+    }
 }
 
+/////////////// Inner class to store roads ///////////////
 class Road {
     start: vec2;
     end: vec2;
